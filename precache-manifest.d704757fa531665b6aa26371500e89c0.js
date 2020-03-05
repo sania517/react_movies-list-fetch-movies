a8f32be94cd1535242642702d8e160718b105428
Olexander <kravchenko.sania@gmail.com>
2020-03-05 18:22:33 +0200

    Updates

---
+++ precache-manifest.d704757fa531665b6aa26371500e89c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a270b51b357bf42689e47af59a55d403",
+    "url": "./index.html"
+  },
+  {
+    "revision": "49caa5cc2aa9d2ff6e2c",
+    "url": "./static/css/2.aeed1b96.chunk.css"
+  },
+  {
+    "revision": "2ff04fc394a013cab458",
+    "url": "./static/css/main.d9112803.chunk.css"
+  },
+  {
+    "revision": "49caa5cc2aa9d2ff6e2c",
+    "url": "./static/js/2.f7117187.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "./static/js/2.f7117187.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ff04fc394a013cab458",
+    "url": "./static/js/main.f1f0641e.chunk.js"
+  },
+  {
+    "revision": "950315c42b3e0eca8ca2",
+    "url": "./static/js/runtime-main.6df99021.js"
+  }
+]);
